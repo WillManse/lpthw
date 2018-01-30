41aa48b78751be201c3d97453c488e2bf101eaef
Will Mansell <Wombat2000@yahoo.com>
2018-01-30 14:23:19 +0000

    seven

---
+++ ex7.py
@@ -0,0 +1,19 @@
+print ("Mary had a little lamb")
+print ("Its fleece was white as {}".format('snow'))
+print ("and every where that Mary went")
+print ("." * 10)
+
+end1 = "L"
+end2 = "e"
+end3 = "a"
+end4 = "r"
+end5 = "n"
+end6 = "P"
+end7 = "y"
+end8 = "t"
+end9 = "h"
+end10 = "o"
+end11 = "n"
+
+print( end1 + end2 + end3 + end4 + end5, end = ' ')
+print( end6 + end7 + end8 +  end9 + end10 + end11)
