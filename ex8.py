18f1c1198d5aa366285e61f4741319fdce7ce86f
Will Mansell <Wombat2000@yahoo.com>
2018-01-30 14:33:55 +0000

    eight

---
+++ ex8.py
@@ -0,0 +1,12 @@
+formatter = "{} {} {} {}"
+print(formatter.format(1, 2, 3, 4 ))
+print(formatter.format("first", "second", "third", "fourth"))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(True, False, "test", 7))
+
+print(formatter.format(
+"line",
+"line with space ",
+"next line",
+"last line"
+)
