2ad2577f5a1a20c06a2fa0084923accc1929f1b6
Will Mansell <Wombat2000@yahoo.com>
2018-01-30 14:06:06 +0000

    first few

---
+++ ex4.py
@@ -0,0 +1,13 @@
+my_name = 'Will'
+my_height = 30
+my_weight = 20
+my_age = 39
+my_eyes = 'purple'
+my_teeth = 'green'
+print(f"Lets talk about {my_name}.")
+print(f"He is {my_height} tall.")
+print(f"He weighs {my_weight}.")
+print(f"His eyes are {my_eyes}.")
+print(f"His  teeth are {my_teeth}.")
+total = my_height + my_weight + my_age
+print(f"total is {total}.")
